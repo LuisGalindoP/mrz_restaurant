917e7ff01ff5326aababae2d1d512536880070f6
luisfgp80 <luisfgp80@gmail.com>
2022-07-19 17:32:41 -0400

    main client, tailwindcss and two components added

---
+++ client/src/components/mainHero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const MainHero = () => {
+
+    function reveal() {
+        let reveals = document.querySelectorAll(".reveal")
+    }
+
+    return (
+        <div>
+
+        </div>
+        )
+}
+
+export default MainHero;
---
+++ client/src/components/topNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopNav = () => {
+    return (
+        <div className='bg-gradient-to-b from-[#3d3d3d] to-[#3d3d3d] h-20 flex flex-auto items-center justify-center gap-6'>
+            <h4 className='text-white font-bebas text-2xl'>MENU</h4>
+            <h4 className='text-white font-bebas text-2xl'>LOCATIONS</h4>
+            <h4 className='text-white font-bebas text-2xl'>ORDER ONLINE</h4>
+            <h4 className='text-white font-bebas text-2xl'>GIFT CARDS</h4>
+            <h4 className='text-white font-bebas text-2xl'>JOBS</h4>
+        </div>
+    )
+}
+
+export default TopNav;
