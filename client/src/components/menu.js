75f7db134fae9e898a830fafd0d685e9908ea8ad
luis <luisfgp80@gmail.com>
2022-09-01 19:02:20 -0400

    Version one page only before adding menu component

---
+++ client/src/components/menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+
+const Menu = (props) => {
+    return (
+        <div>
+            MENU COMPONENT 
+        </div>
+    )
+}
+
+export default Menu;
